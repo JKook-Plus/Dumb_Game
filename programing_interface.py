611773ccaec1930203fb7b517701df31e79fb9e6
JKookaburra <JonahKerlin@gmail.com>
2019-02-26 20:24:09 +1100

    Update 1.2

--- programing_interface.py
+++ programing_interface.py
@@ -6,22 +6,28 @@ fg = "#e5e5e5"
 button_bg = "#46664f"
 button_fg = "#e5e5e5"
 
+
 class Root(Tk):
     def __init__(self, *args, **kwords):
         super().__init__(*args, **kwords)
         self.current = 0
         self.windows = []
+        self.prevpage = None
 
     def append(self, arg):
         self.windows.append(arg)
         return arg
 
     def next(self):
-        if self.current >= len(self.windows):
-            self.current += 1
+        self.current += 1
     def back(self):
-        if not self.current <= 0:
-            self.current -= 1
+        self.current -= 1
+
+    def show(self):
+        if not self.current == self.prevpage:
+            self.windows[self.current].pack_forget()
+        self.windows[self.current].show()
+        self.prevpage = self.current
 
 
 class window(Frame):
@@ -30,6 +36,9 @@ class window(Frame):
         self.menubar = Menu(root)
         self.menubar.add_command(label="Hello!", command=lambda : print("Hello!"))
         self.menubar.add_command(label="Quit!")
+
+        self.menubar.add_command(label="Next", command=root.next)
+        self.menubar.add_command(label="Back", command=root.back)
 
     def show(self):
         root.config(menu=self.menubar)
@@ -42,11 +51,9 @@ class code_interpreter(window):
 
     def __init__(self, root, *args, **kwords):
         super().__init__(root, *args, **kwords)
-        self.main = Text(fg=fg,bg=bg)
-        self.submit = Button(text="Submit", fg=button_fg, bg=button_bg, pady=2, borderwidth=0, height=1, command=lambda : self._submit(self.main))
-        self.label = Label(fg=fg,bg=bg, text="\n\n\n", height = 3)
-
-
+        self.main = Text(root, fg=fg,bg=bg)
+        self.submit = Button(root, text="Submit", fg=button_fg, bg=button_bg, pady=2, borderwidth=0, height=1, command=lambda : self._submit(self.main))
+        self.label = Label(root, fg=fg,bg=bg, text="\n\n\n", height = 3)
 
 
     def show(self):
@@ -57,18 +64,31 @@ class code_interpreter(window):
         self.main.pack(fill=BOTH, expand=True)
         self.label.pack(fill=BOTH)
 
+class test(window):
+    def __init__(self, root, *args, **kwords):
+        super().__init__(root, *args, **kwords)
+        self.main = Label(root, fg=fg,bg=bg, text = "test")
+
+    def show(self):
+        super().show()
+
+        self.main.pack(fill=BOTH, expand=True)
+
 
 
 if __name__ == "__main__":
     root = Root()
 
     def loop():
+        root.show()
         root.after(1000, loop)
 
     root.bind("<F11>", lambda x : root.attributes("-fullscreen", True))
 
-    window = root.append(code_interpreter(root))
-    window.show()
+    coder = root.append(code_interpreter(root))
+    ts = root.append(test(root))
+
+
 
     loop()
     mainloop()
